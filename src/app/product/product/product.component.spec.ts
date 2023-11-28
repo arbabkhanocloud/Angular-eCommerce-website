f216ec7dcb9294f391261e6ca15a4db414bf96c6
Arbab Khan <arbab.tufail@ocloudsolutions.net>
2023-11-28 12:00:20 +0500

    feat: added test cases for product component

---
+++ src/app/product/product/product.component.spec.ts
@@ -0,0 +1,77 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ProductComponent } from './product.component';
+import { MaterialModule } from 'src/app/material/material/material.module';
+import { By } from '@angular/platform-browser';
+
+describe('ProductComponent', () => {
+  let component: ProductComponent;
+  let fixture: ComponentFixture<ProductComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProductComponent],
+      imports: [MaterialModule],
+    });
+
+    fixture = TestBed.createComponent(ProductComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display product information', () => {
+    const product = {
+      id: 0,
+      name: 'Test Product',
+      category: 'Test Category',
+      src: 'test-image.jpg',
+      price: 19.99,
+      description: 'asdf',
+    };
+
+    component.product = product;
+    fixture.detectChanges();
+
+    const titleElement = fixture.debugElement.query(
+      By.css('mat-card-title'),
+    ).nativeElement;
+    const subtitleElement = fixture.debugElement.query(
+      By.css('mat-card-subtitle'),
+    ).nativeElement;
+    const imageElement = fixture.debugElement.query(
+      By.css('img'),
+    ).nativeElement;
+    const priceElement = fixture.debugElement.query(
+      By.css('mat-card-content span'),
+    ).nativeElement;
+
+    expect(titleElement.textContent).toContain(product.name);
+    expect(subtitleElement.textContent).toContain(product.category);
+    expect(imageElement.src).toContain(product.src);
+    expect(priceElement.textContent).toContain('19.99');
+  });
+
+  it('should emit onAdd event', () => {
+    const product = {
+      id: 1,
+      name: 'Test Product',
+      category: 'Test Category',
+      src: 'test-image.jpg',
+      price: 19.99,
+      description: 'asdfsdfsdf',
+    };
+
+    spyOn(component, 'onAdd');
+    component.product = product;
+    fixture.detectChanges();
+
+    const addButton = fixture.debugElement.query(
+      By.css('.add-btn'),
+    ).nativeElement;
+    addButton.click();
+
+    expect(component.onAdd).toHaveBeenCalledWith(product);
+  });
+});
